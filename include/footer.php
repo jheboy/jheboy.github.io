4d266ba91b3a500cef0630b871fb32ea0032f7f0
jheboy <39071533+jheboy@users.noreply.github.com>
2018-06-16 03:23:04 -0700

    index header footer
    php

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+	<footer>
+		<div class="container">
+			<p class="text-center">© 2018 Jessie Boy S. Avellana. All rights reserved.</p>
+		</div>
+	</footer>
+
+	<script src="assets/plugins/jquery/jquery.min.js"></script>
+	<script src="assets/plugins/jquery/jquery-1.12.4.min.js"></script>	
+	<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/jquery/script.js"></script>
+	<script src="assets/plugins/Simple-Fast-Popup-Window-Plugin-For-jQuery-simple-popup-js/dist/jquery.simple-popup.min.js"></script>
+
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Jhay-b Avellana</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/contents/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link href="assets/plugins/Simple-Fast-Popup-Window-Plugin-For-jQuery-simple-popup-js/dist/jquery.simple-popup.min.css" rel="stylesheet">
+  <link href="assets/plugins/Simple-Fast-Popup-Window-Plugin-For-jQuery-simple-popup-js/dist/jquery.simple-popup.settings.css" rel="stylesheet">
+  <link href="assets/contents/css/circle.css" rel="stylesheet">
+</head>
+<body>
+
+  <section class="header">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#Navbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="../pweb/">Jhay-B</a>
+        </div>
+        <div class="collapse navbar-collapse" id="Navbar">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../pweb/">HOME</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#portfolio">PORTFOLIO</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </section>
